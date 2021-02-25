5e7d79b6b61cf0dd23acc3f98df327ea748d8b7a
Michael Fischer <79065779+mfischercodes@users.noreply.github.com>
2021-02-25 15:13:16 -0800

    Update web_scraping.py

--- web_scraping.py
+++ web_scraping.py
@@ -3,18 +3,11 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-url = 'https://www.amazon.ca/0-5meter-Lead-Snowkids-Compatible-Ethernet-Function/dp/B0839CBWBR/ref=br_msw_pdt-4?_encoding=UTF8&smid=A3M67XBJECTJN0&pf_rd_m=A3DWYIK6Y9EEQB&pf_rd_s=&pf_rd_r=B0DJT26VPXYR82JWH3SP&pf_rd_t=36701&pf_rd_p=4500e888-b5b9-4e64-8bd4-fc15289d25f6&pf_rd_i=desktop'
-'''
-
-with open('start.html','r') as html_file:
-    content = html_file.read()
-
-    soup = BeautifulSoup(content, 'lxml')
-    tags = soup.find_all('h5')
-    print(tags)
 '''
 url = 'https://www.amazon.ca/0-5meter-Lead-Snowkids-Compatible-Ethernet-Function/dp/B0839CBWBR/ref=br_msw_pdt-4?_encoding=UTF8&smid=A3M67XBJECTJN0&pf_rd_m=A3DWYIK6Y9EEQB&pf_rd_s=&pf_rd_r=B0DJT26VPXYR82JWH3SP&pf_rd_t=36701&pf_rd_p=4500e888-b5b9-4e64-8bd4-fc15289d25f6&pf_rd_i=desktop'
 
+
+#ONLINE ACCESS
 chrome_options = Options()
 chrome_options.add_argument("--headless") #option don't open browser to the user run in background
 
@@ -25,8 +18,17 @@ soup = BeautifulSoup(html, 'lxml') #variable beautiful soup accessing the html s
 #soup = BeautifulSoup(html, 'html.parser')
 
 tags = soup.find(id ="productTitle").text.strip('\n') #run find or find_all, etc... to find information you like
+'''
 
-print(tags)
+
+
+with open('start.html','r') as html_file:
+    content = html_file.read()
+
+    soup = BeautifulSoup(content, 'lxml')
+    tags = soup.find_all('h5')
+    for i in tags:
+        print(i.text)
 
 
 
